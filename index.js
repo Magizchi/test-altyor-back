7487c67d295e3ffd5ddffe79689c3dd3f21cb72b
Rajanan <g.rajan1993@gmail.com>
2019-04-24 22:57:33 +0200

    'test'

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const Carte = require('./models/model');
-
+const MONGODB_URI = 'mongodb://heroku_l2ft4tzz:3b0b5m28k4qdkd6t55jg3n5tuo@ds147096.mlab.com:47096/heroku_l2ft4tzz';
 const mongoose = require('mongoose');
 mongoose.connect(
 	MONGODB_URI || 'mongodb://localhost/Carte',
